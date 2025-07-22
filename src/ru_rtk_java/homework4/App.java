1a39b09c82135eb17089f89fc3a70b31a9bbfd1e
Naddy777 <ng-y@yandex.ru>
2025-07-23 01:26:57 +0300

    ДЗ про Телевизор

---
+++ src/ru_rtk_java/homework4/App.java
@@ -0,0 +1,7 @@
+package ru_rtk_java.homework4;
+import ru_rtk_java.homework4.Telewisor;
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ru_rtk_java/homework4/Telewisor.java
@@ -0,0 +1,10 @@
+package ru_rtk_java.homework4;
+
+public class Telewisor {
+    short Diagonal;// Диагональ телевизора: 43, 55, 50
+    short RefreshRate;//Частота обновления: 60, 120
+    String ScreenResolution;// разрешение экрана: 1920х1080, 3840х2160, 7680х4320
+    String Mstrix; //Тип матрицы: TN, IPS, VA, OLED
+    boolean SmartTV; //Наличие смарт ТВ
+
+}
