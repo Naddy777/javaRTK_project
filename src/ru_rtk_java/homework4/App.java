6df12bf0bd7a8faccf7813e7e466e551c23355e2
Naddy777 <ng-y@yandex.ru>
2025-07-24 01:05:36 +0300

    + ДЗ про Телевизор

--- src/ru_rtk_java/homework4/App.java
+++ src/ru_rtk_java/homework4/App.java
@@ -1,7 +1,15 @@
 package ru_rtk_java.homework4;
-import ru_rtk_java.homework4.Telewisor;
+
 public class App {
     public static void main(String[] args) {
+        Telewisor telewisor1 = new Telewisor("LG",  60,true,50_000);
+        Telewisor telewisor2 = new Telewisor("Sony",  120,true,200_000);
+        Telewisor telewisor3 = new Telewisor("Samsung",  120,true,100_000);
+        Telewisor telewisor4 = new Telewisor ();
+        Telewisor telewisor5 = new Telewisor("",  0,false,100_000);
 
+        System.out.printf("telewisor1: %s%n", telewisor1);
+        System.out.println("telewisor2: " + telewisor2);
+        System.out.printf("telewisor4: %s%n", telewisor4);
     }
 }
--- src/ru_rtk_java/homework4/Telewisor.java
+++ src/ru_rtk_java/homework4/Telewisor.java
@@ -1,10 +1,23 @@
 package ru_rtk_java.homework4;
 
 public class Telewisor {
-    short Diagonal;// Диагональ телевизора: 43, 55, 50
-    short RefreshRate;//Частота обновления: 60, 120
-    String ScreenResolution;// разрешение экрана: 1920х1080, 3840х2160, 7680х4320
-    String Mstrix; //Тип матрицы: TN, IPS, VA, OLED
-    boolean SmartTV; //Наличие смарт ТВ
+    private String model;// Название, модель
+    private int refreshRate;//Частота обновления: 60, 120
+    private boolean smartTV; //Наличие смарт ТВ
+    private int price;// цена телевизора
+
+    public Telewisor (String model, int refreshRate, boolean smartTV, int price){
+        this.model = model;
+        this.price = price;
+        this.refreshRate = refreshRate;
+        this.smartTV = smartTV;
+    }
+
+    public Telewisor (){
+        this.model = "Не определено";
+        this.price = 49_999;
+        this.refreshRate = 0;
+        this.smartTV = false;
+    }
 
 }
