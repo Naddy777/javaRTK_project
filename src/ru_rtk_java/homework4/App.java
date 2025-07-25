f1bf0143e0e9f3acf468b81bf0d5be51e8c89991
Naddy777 <ng-y@yandex.ru>
2025-07-25 11:49:14 +0300

    + ДЗ про Телевизор, добавлен ввод с клавиатуры

--- src/ru_rtk_java/homework4/App.java
+++ src/ru_rtk_java/homework4/App.java
@@ -1,15 +1,34 @@
 package ru_rtk_java.homework4;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Telewisor telewisor1 = new Telewisor("LG",  60,true,50_000);
-        Telewisor telewisor2 = new Telewisor("Sony",  120,true,200_000);
-        Telewisor telewisor3 = new Telewisor("Samsung",  120,true,100_000);
-        Telewisor telewisor4 = new Telewisor ();
-        Telewisor telewisor5 = new Telewisor("",  0,false,100_000);
+        Telewisor telewisor1 = new Telewisor("LG",  60,"есть",50_000);
+        telewisor1.SetTV();
+        Telewisor telewisor2 = new Telewisor("Sony",  120,"есть",200_000);
+        telewisor2.SetTV();
+        Telewisor telewisor3 = new Telewisor("Samsung",  120,"есть",100_000);
+        telewisor3.SetTV();
+        Telewisor telewisor5 = new Telewisor("Российский аналог",  60,"нет",100_000);
+        telewisor5.SetTV();
 
-        System.out.printf("telewisor1: %s%n", telewisor1);
-        System.out.println("telewisor2: " + telewisor2);
-        System.out.printf("telewisor4: %s%n", telewisor4);
+        Scanner scanner = new Scanner(System.in);// создаем телевизор через ввод с клавиатуры
+        System.out.print("Введите модель телевизора: ");
+        String model1 = scanner.nextLine();
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Введите частоту обновления: ");
+        int refreshRate1 = scanner2.nextInt();
+
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.print("Введите наличие smartTV: есть/нет: ");
+        String smartTV1 = scanner3.nextLine();
+
+        Scanner scanner4 = new Scanner(System.in);
+        System.out.print("Введите цену телевизора: ");
+        int price1 = scanner4.nextInt();
+
+        Telewisor telewisor6 = new Telewisor(model1,  refreshRate1, smartTV1, price1);
+        telewisor6.SetTV();
     }
 }
--- src/ru_rtk_java/homework4/Telewisor.java
+++ src/ru_rtk_java/homework4/Telewisor.java
@@ -3,21 +3,56 @@ package ru_rtk_java.homework4;
 public class Telewisor {
     private String model;// Название, модель
     private int refreshRate;//Частота обновления: 60, 120
-    private boolean smartTV; //Наличие смарт ТВ
+    private String smartTV; //Наличие смарт ТВ
     private int price;// цена телевизора
 
-    public Telewisor (String model, int refreshRate, boolean smartTV, int price){
+
+        public Telewisor (String model, int refreshRate, String smartTV, int price){
         this.model = model;
         this.price = price;
         this.refreshRate = refreshRate;
         this.smartTV = smartTV;
     }
 
-    public Telewisor (){
-        this.model = "Не определено";
-        this.price = 49_999;
-        this.refreshRate = 0;
-        this.smartTV = false;
+    public String getModel() {
+        return model;
     }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getRefreshRate() {
+        return refreshRate;
+    }
+
+    public void setRefreshRate(int refreshRate) {
+        this.refreshRate = refreshRate;
+    }
+
+    public String isSmartTV() {
+        return smartTV;
+    }
+
+    public void setSmartTV(String smartTV) {
+        this.smartTV = smartTV;
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+
+    }
+
+    public void SetTV(){
+
+        System.out.println("Вы создали телевизор: модель - " + model + ", частота обновления - " + refreshRate + ", функция smart - " + smartTV + ", цена = " + price);
+    }
+
+
 
 }
