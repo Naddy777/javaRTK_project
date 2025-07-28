1ac169824261da1627c36558e02b5407812a50a7
Naddy777 <ng-y@yandex.ru>
2025-07-28 14:57:58 +0300

    + ДЗ 5 добавлены три решения

--- src/ru_rtk_java/homework5/DZ5_1.java
+++ src/ru_rtk_java/homework5/DZ5_1.java
@@ -13,20 +13,26 @@ import java.util.Scanner;
 буквы, с учетом замкнутости клавиатуры
 
  */
-public class DZ5 {
+public class DZ5_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Создаем объект Сканер
         System.out.println("Введите символ английского алфавита: ");
         String simvol = scanner.nextLine();
         char letter= simvol.charAt(0);// берем из введенной строки первый символ
         System.out.println("Введенная буква: "+letter);
-        String klaviatura="qwertyuiopasdfghjklzxcvbnm";// определяем индекс введенного символа
-        int index = klaviatura.indexOf(letter);
+        String klaviatura="qwertyuiopasdfghjklzxcvbnm";
+        System.out.println("Длина абракадабры: " + klaviatura.length());
+        int index = klaviatura.indexOf(letter);// определяем индекс введенного символа  в стринге klaviatura
         System.out.println("Индекс введенной буквы: "+index);
-        if (index ==-1){// вводим для того, чтобы отразить символ вне списка
+        if (index ==-1){// вводим для того, чтобы отразить символ вне списка (работает по типу - надо вывести сообщение об ошибке, поэтому делаем проверку
+            // на тот индекс, который вне списка. Если он в списке, то ошибка не выводится)
             System.out.println("Ошибка! Не тот символ ");
         }else {
-            int leftIndex=(index-1+klaviatura.length()) % klaviatura.length();// остатов от деления - без него не работает
+           int leftIndex=(index-1+klaviatura.length()) % klaviatura.length();// остаток от деления нужен чтобы зациклить klaviatura и
+            // чтобы индекс не вываливался за пределы списка
+            //int leftIndex=(index-1);// так не работает для q, не зацикливает klaviatura
+           // int leftIndex=(index-1+klaviatura.length()); // так работает для q, но не работает для остальных
+            // букв (они выпадают за пределы списка по индексам, если прибавлять длину списка)
             char leftSymbol=klaviatura.charAt(leftIndex);
             System.out.println("Буква слева на клавиатуре: " + leftSymbol);
         }
--- src/ru_rtk_java/homework5/DZ5_3.java
+++ src/ru_rtk_java/homework5/DZ5_3.java
@@ -18,11 +18,11 @@ public class DZ5_3 {
         System.out.println("Введите два слова через пробел: ");
         String liniya = scanner.nextLine();
         System.out.println("Введенная строка: " + liniya);
-        String lowerLinya = liniya.toLowerCase();
+        String lowerLinya = liniya.toLowerCase();// переводим в нижний регистр
         System.out.println("Все в нижнем регистре: " + lowerLinya);
-        String[] words=lowerLinya.split(" ");// сплитуем по пробелу
+        String[] words=lowerLinya.split(" ");// делаем массив, сплитуем по пробелу
         if (words.length!=2){
-            System.out.println("Ошибка! введите одно слово: ");
+            System.out.println("Ошибка! Условие не выполнено! ");
             scanner.close();
             return;
         }
@@ -30,13 +30,13 @@ public class DZ5_3 {
         for (int i = 0; i < words.length; i++) {
             String word = words[i];
             System.out.println("Текущее слово перед сортировкой: " + word);
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
-            String sortedWord = new String(chars);
+            char[] chars = word.toCharArray();// перекладываем в массив символов
+            Arrays.sort(chars);//сортируем массив символов
+            String sortedWord = new String(chars);// собираем из отсортированных символов новый стринг chars
             System.out.println("Отсортированное слово: " + sortedWord);
             result.append(sortedWord);
             if (i < words.length - 1) {
-                result.append(" ");
+                result.append(" ");// мы добавляем в конце нового слова пробел (к результату добавить пробел)
             }
         }
         System.out.println("Резльтат: " + result.toString());
@@ -44,34 +44,4 @@ public class DZ5_3 {
     }
 }
 
-//
-//        public static void main(String[] args) {
-//            Scanner scanner= new Scanner(System.in);
-//            System.out.println("Введите два слова через пробел:");
-//            String line=scanner.nextLine();
-//            System.out.println("Введенная строка: "+line);
-//            String lowerLine=line.toLowerCase();
-//            System.out.println("Нижний регистр: "+lowerLine);
-//            String[] words=lowerLine.split(" ");
-//            if(words.length!=2){
-//                System.out.println("Ошибка, требуется ввести ровно два слова");
-//                scanner.close();
-//                return;
-//            }
-//            StringBuilder result= new StringBuilder();
-//            for (int i=0;i< words.length;i++){
-//                String word=words[i];
-//                System.out.println("Текущее слово перед сортировкой: "+word);
-//                char[] chars=word.toCharArray();
-//                Arrays.sort(chars);
-//                String sortedWord=new String(chars);
-//                System.out.println("Отсортированное слово: "+sortedWord);
-//                result.append(sortedWord);
-//                if(i<words.length-1) {
-//                    result.append(" ");
-//                }
-//            }
-//            System.out.println("Резльтат: "+result.toString());
-//            scanner.close();
-//        }
-//}
+
